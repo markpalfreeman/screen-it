78054ab1c9c90546ba971e8098dd500195d7e5f7
Mark Palfreeman <mapalfre@microsoft.com>
2023-01-17 06:57:38 -0800

    remove premature test

--- src/components/App/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { App } from "./";
-
-test("renders heading element", () => {
-  render(<App />);
-  const heading = screen.getByText("Screen it");
-  expect(heading).toBeInTheDocument();
-});
